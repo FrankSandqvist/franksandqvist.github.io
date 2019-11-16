64048f6509ceba28b380bcf0b055d634f0f99440
Frank Sandqvist <kankki@gmail.com>
2019-11-17 01:47:41 +0200

    ...

---
+++ precache-manifest.4384f88d91f35240d5e7596872d356d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc43f42eec93e72ab7aa0e1353acd0f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536c9d5c6ffc17534fe7",
+    "url": "/static/css/main.ba2f9470.chunk.css"
+  },
+  {
+    "revision": "801282683707de6ae4ee",
+    "url": "/static/js/2.c4a70e02.chunk.js"
+  },
+  {
+    "revision": "536c9d5c6ffc17534fe7",
+    "url": "/static/js/main.9b716fbd.chunk.js"
+  },
+  {
+    "revision": "755c33095b0bbb10c48b",
+    "url": "/static/js/runtime-main.7577a23e.js"
+  }
+]);
