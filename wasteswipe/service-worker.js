2256b5d41c9c13cc31a3f314d3cb1d7693cfe6b8
Frank Sandqvist <kankki@gmail.com>
2019-11-17 08:55:29 +0200

    ...

--- wasteswipe/service-worker.js
+++ wasteswipe/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.149c66c7d1ad57963fb3cef86ca6437a.js"
+  "/wasteswipe/precache-manifest.8c34e4d19dbf82dba59cc52ac2a65514.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wasteswipe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
